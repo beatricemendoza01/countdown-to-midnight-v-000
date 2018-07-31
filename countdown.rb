1d5fae34a9032d6665f22816173acf868c95396d
Beatrice Mendoza <mendoza@nyu.edu>
2018-07-31 01:33:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while seconds > 0
+    "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
